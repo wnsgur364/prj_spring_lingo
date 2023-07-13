82b7edbd181d533ff443365e228083d87c715168
wnsgur364 <wnsgur364@naver.com>
2023-07-13 16:21:47 +0900

    관리자 템플릿 추가

---
+++ src/main/java/com/lingo/app/admin/AdminController.java
@@ -0,0 +1,19 @@
+package com.lingo.app.admin;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class AdminController {
+
+	@RequestMapping ("/indexAdmin")
+	public String indexAdmin() {
+		return "/infra/admin/modules/indexAdmin";
+	}
+	
+	@RequestMapping ("/tables")
+	public String tables() {
+		return "/infra/admin/modules/tables";
+	}
+			
+}
