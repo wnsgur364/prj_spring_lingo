2ad73e50a9b9f128c40b7c0636e47b15eb30434b
DohyeongAn <rrgg0204@naver.com>
2023-07-27 15:26:51 +0900

    오류수정

--- src/main/java/com/lingo/app/code/CodeController.java
+++ src/main/java/com/lingo/app/code/CodeController.java
@@ -4,12 +4,5 @@ import org.springframework.stereotype.Controller;
 
 @Controller
 public class CodeController {
-<<<<<<< HEAD
-	
 
-=======
-		
->>>>>>> branch 'main' of https://github.com/wnsgur364/prj_spring_lingo.git
-	 
-	
 }
--- src/main/java/com/lingo/app/code/CodeDao.java
+++ src/main/java/com/lingo/app/code/CodeDao.java
@@ -13,32 +13,18 @@ public class CodeDao {
 	
 	@Inject
 	@Resource(name = "sqlSession")
-<<<<<<< HEAD
 	private  SqlSession sqlSession;
-=======
-	private SqlSession sqlSession;
->>>>>>> branch 'main' of https://github.com/wnsgur364/prj_spring_lingo.git
-	
+
 	private static String namespace = "com.lingo.app.code.CodeMapper";
 
-<<<<<<< HEAD
 	public  int selectOneCount(CodeVo vo){ return sqlSession.selectOne(namespace + ".selectOneCount", vo); }
 	public  List<Code> selectList(CodeVo vo) {return sqlSession.selectList(namespace + ".selectList", vo); }	
 	public  Code selectOne(CodeVo vo){ return sqlSession.selectOne(namespace + ".selectOne", vo); }
-	public  int updatse(Code dto){ return sqlSession.update(namespace + ".update", dto); }
+	public  int update(Code dto){ return sqlSession.update(namespace + ".update", dto); }
 	public  int delete(Code dto){ return sqlSession.delete(namespace + ".delete", dto); }
 	public  int uelete(Code dto){ return sqlSession.update(namespace + ".uelete", dto); }
 	public  int insert(Code dto){ return sqlSession.insert(namespace + ".insert", dto); }
-=======
-	public int selectOneCount(CodeVo vo){ return sqlSession.selectOne(namespace + ".selectOneCount", vo); }
-	public List<Code> selectList(CodeVo vo) {return sqlSession.selectList(namespace + ".selectList", vo); }	
-	public Code selectOne(CodeVo vo){ return sqlSession.selectOne(namespace + ".selectOne", vo); }
-	public int update(Code dto){ return sqlSession.update(namespace + ".update", dto); }
-	public int delete(Code dto){ return sqlSession.delete(namespace + ".delete", dto); }
-	public int uelete(Code dto){ return sqlSession.update(namespace + ".uelete", dto); }
-	public int insert(Code dto){ return sqlSession.insert(namespace + ".insert", dto); }
->>>>>>> branch 'main' of https://github.com/wnsgur364/prj_spring_lingo.git
-	
+
 //	for cache
 	public List<Code> selectListCachedCodeArrayList(){ return sqlSession.selectList(namespace + ".selectListCachedCodeArrayList", null); }
 }
