e2e39baa2095b2747ea109933cf6f88ad7b4bddf
wnsgur364 <wnsgur364@naver.com>
2023-08-08 17:12:01 +0900

    퀴즈 여전히 ...

--- src/main/java/com/lingo/app/quiz/QuizController.java
+++ src/main/java/com/lingo/app/quiz/QuizController.java
@@ -1,5 +1,9 @@
 package com.lingo.app.quiz;
 
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Random;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
@@ -16,11 +20,39 @@ public class QuizController {
 	@Autowired
 	AnswerServiceImpl aService;
 	
-	@RequestMapping("/quiz")
-	public String quiz(QuizVo qvo, Model qModel, AnswerVo avo, Model aModel) {
-		qModel.addAttribute("quiz", qService.selectList(qvo));
-		aModel.addAttribute("answer", aService.selectList(avo));
-		
-		return "/infra/user/index/quiz";
-	}
+    @RequestMapping("/quiz")
+    public String quiz(QuizVo qvo, Model qModel, AnswerVo avo, Model aModel) {
+        List<Quiz> allQuizzes = qService.selectList(qvo);
+
+        List<Quiz> selectedQuizzes = new ArrayList<>();
+
+        // Assuming allQuizzes and allAnswers have the same size and corresponding indices match
+
+        int totalQuestions = allQuizzes.size();
+        int numQuestionsToShow = 20;
+
+        // Ensure we have enough questions to show
+        if (totalQuestions >= numQuestionsToShow) {
+            Random random = new Random();
+            List<Integer> selectedIndices = new ArrayList<>();
+
+            while (selectedIndices.size() < numQuestionsToShow) {
+                int randomIndex = random.nextInt(totalQuestions);
+                
+                // Avoid duplicate questions
+                if (!selectedIndices.contains(randomIndex)) {
+                    selectedIndices.add(randomIndex);
+                    selectedQuizzes.add(allQuizzes.get(randomIndex));
+                }
+            }
+        } else {
+            // Handle the case where there are not enough questions
+            // You can show a message or take a different action here
+        }
+
+        qModel.addAttribute("quiz", selectedQuizzes);
+        aModel.addAttribute("answer", aService.selectList(avo));
+
+        return "/infra/user/index/quiz";
+    }
 }
