7dee9b3f95c057673903f7e1c8c3e01af747a4b4
wnsgur364 <wnsgur364@naver.com>
2023-08-24 21:19:55 +0900

    퀴즈 컨트롤러 겹친거 수정

--- src/main/java/com/lingo/app/quiz/QuizController.java
+++ src/main/java/com/lingo/app/quiz/QuizController.java
@@ -10,11 +10,7 @@ import javax.servlet.http.HttpSession;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-<<<<<<< HEAD
-=======
 import org.springframework.web.bind.annotation.ModelAttribute;
-import org.springframework.web.bind.annotation.RequestBody;
->>>>>>> branch 'main' of https://github.com/wnsgur364/prj_spring_lingo.git
 import org.springframework.web.bind.annotation.RequestMapping;
 
 import com.lingo.app.answer.AnswerServiceImpl;
