7e32b1f7dd41c02a4203c58c5e011d2ff34108e5
wnsgur364 <wnsgur364@naver.com>
2023-08-25 17:33:53 +0900

    STOMP 초기 작업 클래스 생성

---
+++ src/main/java/com/lingo/app/chat/Message.java
@@ -0,0 +1,15 @@
+package com.lingo.app.chat;
+
+import java.util.Date;
+import lombok.Data;
+
+@Data
+public class Message {
+	private String message;
+	private String nickname;
+	private Date date;
+ 
+	Message(){
+		date = new Date();
+	}
+}
